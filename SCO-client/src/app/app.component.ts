235c3fd39d695ceb21f60baa6e57b4ab76678073
marco-battisti-04 <156814368+marco-battisti-04@users.noreply.github.com>
2024-02-01 12:37:38 +0000

    updated app component, added routes

--- SCO-client/src/app/app.component.ts
+++ SCO-client/src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'SCO-client';
+  title = 'SCO Karma Project';
+  
+  constructor(private titleService:Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
--- SCO-client/src/app/app.routes.ts
+++ SCO-client/src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "user",
+        component: ProfileComponent
+    }
+
+];
